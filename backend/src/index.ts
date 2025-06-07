3a60886aa20ca38b8b9f9350e15da04b726f8a09
sourav <mailsourav014@gmail.com>
2025-06-07 23:38:05 +0530

    completed basic websocket

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,25 @@
 import http from "http"
 import { Socket } from "socket.io";
-const express = require('express');
-const {Server} = require("socket.io");
+import  express  from 'express' ;
+import { Server } from "socket.io";
+import { UserManager } from "./managers/UserManager";
 
 const app= express();
 const server= http.createServer(http);
-const io= new Server(server);
+const io= new Server(server,{
+    cors:{
+        origin:"*"
+    }
+});
+const userManager= new UserManager()
 
 io.on('connection',(socket:Socket)=>{
 console.log("A user connected")
+userManager.addUser("randomuser",socket)
+socket.on("disconnect",()=>{
+    userManager.removeUser(socket.id)
+})
+
 })
 
 server.listen(3000,()=>{
--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -1,12 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { io, Socket } from "socket.io-client";
+const URL = "http://localhost:3000";
 
 export const Room = () => {
   const [searchParams] = useSearchParams();
+  const [socket, setSocket] = useState<null | Socket>(null);
+  const [lobby, setLobby] = useState(true);
+
   const name = searchParams.get("name");
   useEffect(() => {
-    //logic to init user to the room
+    const socket = io(URL);
+    socket.on("send-offer", ({ roomId }) => {
+      setLobby(false);
+      alert("Send offer please");
+      socket.emit("offer", {
+        sdp: "",
+        roomId,
+      });
+    });
+    socket.on("offer", ({ roomId, offer }) => {
+      setLobby(false);
+      alert("Send answer please");
+      socket.emit("answer", {
+        roomId,
+        sdp: "",
+      });
+    });
+    socket.on("answer", ({ roomId, answer }: any) => {
+      setLobby(false);
+      alert("connection done");
+    });
+    socket.on("lobby", () => {
+      setLobby(true);
+    });
+
+    setSocket(socket);
   }, [name]);
 
-  return <div>Hi {name}</div>;
+  if (lobby) {
+    return <div>Finding a match</div>;
+  }
+
+  return (
+    <div>
+      Hi {name}
+      <video width={400} height={400} />
+      <video width={400} height={400} />
+    </div>
+  );
 };
